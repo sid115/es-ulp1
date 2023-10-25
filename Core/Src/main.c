7871dfe36ba99dfdf702a56642272cee249e844e
sid <sid@portuus.de>
2023-10-25 18:22:58 +0200

    test 7 fails

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,17 +23,16 @@
 /* USER CODE BEGIN Includes */
 #include <stdbool.h>
 #include <string.h>
-#include "stm32f4xx_hal.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+typedef uint8_t crc;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define MY_ADDRESS 0x03
+#define MY_ADDRESS 3 /* board number */
 
 #define MMCP_MASTER_ADDRESS 0
 #define MMCP_VERSION 5
@@ -41,23 +40,26 @@
 #define L7_PDU_size 9
 #define L7_SDU_size 8
 #define L7_PCI_size 1
+
 #define L3_PDU_size 13
 #define L3_SDU_size 9
 #define L3_PCI_size 4
+
 #define L2_PDU_size 14
 #define L2_SDU_size 13
 #define L2_PCI_size 1
+
 #define L1_PDU_size 16
 #define L1_SDU_size 14
 #define L1_PCI_size 2
 
-#define _SOF 0
-#define _EOF 0
+#define SOF 0
+#define EOF 0
 
-#define DEBOUNCE_INTERVAL 10 // button debounce time in milliseconds
+#define DEBOUNCE_INTERVAL 10 /* button debounce time in milliseconds */
 
 #define CRC_POLYNOMIAL 0x9b
-#define CRC_WIDTH  (8 * sizeof(uint8_t))
+#define CRC_WIDTH  (8 * sizeof(crc))
 #define CRC_TOPBIT (1 << (CRC_WIDTH - 1))
 /* USER CODE END PD */
 
@@ -71,13 +73,14 @@ UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-uint8_t L1_PDU[L1_PDU_size] = { 0 };
-uint8_t rxBuffer[L1_PDU_size] = { 0 }; /* receive buffer */
-
-uint8_t cnt = 0; /* counts button presses */
-
+/* GLOBALS */
 bool dataReceived = false;
 bool dataTransmitted = false;
+
+uint8_t rxBuffer[L1_PDU_size] = { 0 };
+uint8_t L1_PDU[L1_PDU_size] = { 0 };
+
+uint8_t cnt = 0; /* button press counter */
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -86,20 +89,20 @@ static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
-void AL_UART_RxCpltCallback (UART_HandleTypeDef *huart);
-void HAL_UART_TxCpltCallback (UART_HandleTypeDef *huart);
-void HAL_GPIO_EXTI_Callback (uint16_t GPIO_Pin);
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart);
+void HAL_GPIO_EXTI_Callback ( uint16_t GPIO_Pin );
 
-void L1_receive(uint8_t L1_PDU[]);
-void L2_receive(uint8_t L2_PDU[]);
-void L3_receive(uint8_t L3_PDU[]);
-void L7_receive(uint8_t L7_PDU[]);
 void L1_send(uint8_t L1_SDU[]);
+void L1_receive(uint8_t L1_PDU[]);
 void L2_send(uint8_t L2_SDU[]);
+void L2_receive(uint8_t L2_PDU[]);
 void L3_send(uint8_t L3_SDU[]);
-void L7_send(uint8_t ID, uint8_t L7_SDU[]);
+void L3_receive(uint8_t L3_PDU[]);
+void L7_send(uint8_t ApNr, uint8_t L7_SDU[]);
+void L7_receive(uint8_t L7_PDU[]);
 
-uint8_t crc(const uint8_t message[], size_t nBytes);
+crc crcSlow(uint8_t const message[], int nBytes);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -139,31 +142,25 @@ int main(void)
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
-  /* begin testing */
-  //uint8_t test[L1_PDU_size] =  {0, 3, 0, 5, 0, 100, 43, 9, 3, 2, 1, 0, 99, 100, 183, 0};
-  //memcpy(L1_PDU, test, L1_PDU_size);
-  //dataReceived = true;
-  /* end testing */
-
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
-  {  
+  {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	HAL_UART_Receive_IT(&huart2, rxBuffer, L1_PDU_size);
+    HAL_UART_Receive_IT(&huart2, rxBuffer, L1_PDU_size);
 
     if (dataReceived) {
       L1_receive(L1_PDU);
 
       dataReceived = false;
 
-      while (!dataTransmitted) {};
+      while (!dataTransmitted) {} /* wait for response */
       dataTransmitted = false;
-    }
+	}
   }
   /* USER CODE END 3 */
 }
@@ -313,59 +310,51 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
   memcpy(L1_PDU, rxBuffer, L1_PDU_size);
+
   dataReceived = true;
 }
 
-void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
   dataTransmitted = true;
 }
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
-  unsigned long millis = HAL_GetTick(); // elapsed milliseconds for button debouncing
-  unsigned long lastPress = 0; // time of last rising edge (button press)
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  static unsigned long millis = 0; /* elapsed time in milliseconds */
+  static unsigned long lastPress = 0; /* time since last button press */
 
+  millis = HAL_GetTick();
+  
   if (GPIO_Pin == B1_Pin && (millis - lastPress) > DEBOUNCE_INTERVAL) {
     cnt++;
     lastPress = millis;
   }
 }
 
-uint8_t crc(const uint8_t message[], size_t nBytes) {
-  uint8_t remainder = 0;	
-
-  for (size_t byte = 0; byte < nBytes; ++byte) {
-    remainder ^= (message[byte] << (CRC_WIDTH - 8));
-    for (uint8_t bit = 8; bit > 0; --bit) {
-      if (remainder & CRC_TOPBIT) {
-        remainder = (remainder << 1) ^ CRC_POLYNOMIAL;
-      } else {
-        remainder = (remainder << 1);
-      }
-    }
-  }
-
-  return (remainder);
-}
-
-void L1_send(uint8_t L1_SDU[]) {
-  //uint8_t L1_PDU[L1_PDU_size] = { 0 };
-  uint8_t L1_PCI[L1_PCI_size] = {_SOF, _EOF};
-
-  L1_PDU[0] = L1_PCI[0]; /* SOF */
+void L1_send(uint8_t L1_SDU[])
+{
+  L1_PDU[0] = SOF;
   memcpy(&L1_PDU[1], L1_SDU, L1_SDU_size);
-  L1_PDU[L1_PDU_size - 1] = L1_PCI[1]; /* EOF */
-
+  L1_PDU[L1_PDU_size - 1] = EOF;
+  
   HAL_UART_Transmit_IT(&huart2, L1_PDU, L1_PDU_size);
 }
 
-void L1_receive(uint8_t L1_PDU[]) {
+void L1_receive(uint8_t L1_PDU[])
+{
   uint8_t L1_SDU[L1_SDU_size] = { 0 };
 
   memcpy(L1_SDU, &L1_PDU[1], L1_SDU_size);
@@ -373,44 +362,45 @@ void L1_receive(uint8_t L1_PDU[]) {
   L2_receive(L1_SDU);
 }
 
-void L2_send(uint8_t L2_SDU[]) {
+void L2_send(uint8_t L2_SDU[])
+{
   uint8_t L2_PDU[L2_PDU_size] = { 0 };
   
   memcpy(L2_PDU, L2_SDU, L2_SDU_size);
-  L2_PDU[L2_SDU_size] = crc(L2_SDU, L2_SDU_size); /* checksum */
-
+  L2_PDU[L2_SDU_size] = crcSlow(L2_SDU, L2_SDU_size); /* checksum */
+  
   L1_send(L2_PDU);
 }
 
-void L2_receive(uint8_t L2_PDU[]) {
-  uint8_t checksum = L2_PDU[L2_SDU_size];
+void L2_receive(uint8_t L2_PDU[])
+{
   uint8_t L2_SDU[L2_SDU_size] = { 0 };
-
+  uint8_t checksum = L2_PDU[L2_SDU_size];
+  
   memcpy(L2_SDU, L2_PDU, L2_SDU_size);
-
-  if (checksum == crc(L2_SDU, L2_SDU_size)) {
+  
+  if (checksum == crcSlow(L2_SDU, L2_SDU_size)) {
     L3_receive(L2_SDU);
-  } else { /* discard */
-    Error_Handler();
+  } else {
+    dataTransmitted = true; /* error */
   }
 }
 
-void L3_send(uint8_t L3_SDU[]) {
+void L3_send(uint8_t L3_SDU[])
+{
   uint8_t L3_PDU[L3_PDU_size] = { 0 };
-  uint8_t L3_PCI[L3_PCI_size] = { 0 };
+  
+  L3_PDU[0] = MMCP_MASTER_ADDRESS; /* to */
+  L3_PDU[1] = MY_ADDRESS; /* from */
+  L3_PDU[2] = MMCP_VERSION; /* version */
 
-  L3_PCI[0] = MMCP_MASTER_ADDRESS; /* to */
-  L3_PCI[1] = MY_ADDRESS; /* from */
-  L3_PCI[2] = MMCP_VERSION; /* version */
-  L3_PCI[3] = 0; /* hops */
-
-  memcpy(L3_PDU, L3_PCI, L3_PCI_size);
   memcpy(&L3_PDU[L3_PCI_size], L3_SDU, L3_SDU_size);
-
+  
   L2_send(L3_PDU);
 }
 
-void L3_receive(uint8_t L3_PDU[]) {
+void L3_receive(uint8_t L3_PDU[])
+{
   uint8_t L3_PCI[L3_PCI_size] = { 0 };
   uint8_t L3_SDU[L3_SDU_size] = { 0 };
 
@@ -421,34 +411,30 @@ void L3_receive(uint8_t L3_PDU[]) {
   uint8_t from = L3_PCI[1];
   uint8_t version = L3_PCI[2];
 
-  if (to == 0 && from == 0) Error_Handler(); /* discard */
-  
-  if (to == 0) { /* forward */
-    //cnt++; 
-    L3_PDU[3]++;
-    L2_send(L3_PDU);
-  } 
-
-  if (to == MY_ADDRESS && from == MMCP_MASTER_ADDRESS && version == MMCP_VERSION) { /* pass to next layer */
-    L7_receive(L3_SDU); 
-  } else { /* discard */
-    Error_Handler();
+  if (to != MMCP_MASTER_ADDRESS && from == MMCP_MASTER_ADDRESS && version == MMCP_VERSION) {
+    if (L3_PDU[0] == MY_ADDRESS) {
+      L7_receive(L3_SDU);
+    } else {
+      L3_PDU[3]++; /* cnt++ */
+      L2_send(L3_PDU);
+    }
+  } else {
+    dataTransmitted = true; /* error */
   }
 }
 
-void L7_send(uint8_t ID, uint8_t L7_SDU[]) {
+void L7_send(uint8_t ApNr, uint8_t L7_SDU[])
+{
   uint8_t L7_PDU[L7_PDU_size] = { 0 };
-  uint8_t L7_PCI[L7_PCI_size] = { 0 };
 
-  L7_PCI[0] = ID;
-
-  memcpy(L7_PDU, L7_PCI, L7_PCI_size);
-  memcpy(&L7_PDU[L7_PCI_size], L7_SDU, L7_SDU_size);
+  L7_PDU[0] = ApNr;
+  memcpy(&L7_PDU[1], L7_SDU, L7_SDU_size);
 
   L3_send(L7_PDU);
 }
 
-void L7_receive(uint8_t L7_PDU[]) {
+void L7_receive(uint8_t L7_PDU[])
+{
   uint8_t L7_SDU[L7_SDU_size] = { 0 };
   uint8_t ApNr = L7_PDU[0];
 
@@ -483,13 +469,29 @@ void L7_receive(uint8_t L7_PDU[]) {
       break;
 
     default:
-      Error_Handler();
+      dataTransmitted = true; /* error */
       break;
   }
 
   L7_send(ApNr, L7_SDU);
 }
 
+crc crcSlow(uint8_t const message[], int nBytes) {
+  crc remainder = 0;
+
+  for (int byte = 0; byte < nBytes; ++byte) {
+    remainder ^= (message[byte] << (CRC_WIDTH - 8));
+    for (uint8_t bit = 8; bit > 0; --bit){
+      if (remainder & CRC_TOPBIT) {
+        remainder = (remainder << 1) ^ CRC_POLYNOMIAL;
+      } else {
+        remainder = (remainder << 1);
+      }
+    }
+  }
+
+  return remainder;
+}
 /* USER CODE END 4 */
 
 /**
@@ -500,7 +502,6 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  dataTransmitted = true;
   __disable_irq();
   while (1)
   {
@@ -519,7 +520,8 @@ void Error_Handler(void)
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
